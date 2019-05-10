2ba17312acb07f962f7389616dda37dd71449a9d
Sreeram P <sreeramp96@gmail.com>
2019-05-10 16:23:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ import cv2
 import random
 import math
 import matplotlib.pyplot as plt
-#from firebase import firebase
-#import json
+from firebase import firebase
+import json
 
-#firebase = firebase.FirebaseApplication('https://floodmonitoringsystem-5c2e1.firebaseio.com/', None)
+# firebase = firebase.FirebaseApplication('https://floodmonitoringsystem-5c2e1.firebaseio.com/', None)
 
 scaling_factorx = 0.8
 scaling_factory = 0.8
@@ -24,27 +24,21 @@ height = []
 
 # for capture frame by frame
 nframes = 20
-interval = 10
 flag = 0
-r = None
-for i in range(nframes):
-    ret, frame = cap.read()
-    # Select ROI
-    if flag == 0:
-        r = cv2.selectROI(frame)
-        print(r)
-        frame = frame[int(r[1]):int(r[1] + r[3]), int(r[0]):int(r[0] + r[2])]  # Crop image
-        flag = 1
-    frame = frame[int(r[1]):int(r[1] + r[3]), int(r[0]):int(r[0] + r[2])]  # Crop image
-    cv2.imwrite('./img_' + str(i).zfill(4) + '.png', frame)
-    random_time = interval
 
+ret, frame = cap.read()
+cv2.imwrite("test.jpg", frame)
+im = cv2.imread("test.jpg")
+
+# Select ROI
+r = cv2.selectROI(img=im, windowName="test")
 # if __name__ == '__main__' :
 # im = cv2.imread("image.jpg") # Read image
 # r = cv2.selectROI(im)  # Select ROI
 # imCrop = im[int(r[1]):int(r[1]+r[3]), int(r[0]):int(r[0]+r[2])] # Crop image
 # cv2.imshow("Image", imCrop)# Display cropped image
 # cv2.waitKey(0)
+
 
 while (1):
 
@@ -54,15 +48,13 @@ while (1):
         break
 
     frame = frame[int(r[1]):int(r[1] + r[3]), int(r[0]):int(r[0] + r[2])]
+
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    edges = cv2.Canny(gray, 1, 100)
-    minLineLength = 250
-    maxLineGap = 50
-    lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 15, minLineLength, maxLineGap)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    # Find the edges in the image using canny detector
+    edges = cv2.Canny(gray, 50, 200)
+    # Detect points that form a line
+    lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 100, minLineLength=10, maxLineGap=250)
+    # Draw lines on the image
     if lines is not None:
         for line in lines:
             for x1, x2, y1, y2 in line:
@@ -72,16 +64,16 @@ while (1):
                 length = y1 - y2
                 print(length)
                 height.append(length)
-                #if length < 100:
-                    #data = {'height': length, 'message': 'crtical'}
-                    #result = firebase.post("/floodmonitoringsystem-5c2e1/data/", data)
+                # if length < 100:
+                # data = {'height': length, 'message': 'crtical'}
+                # result = firebase.post("/floodmonitoringsystem-5c2e1/data/", data)
+    cv2.imshow("Output Frame", frame)
 
-cv2.imshow("Output Frame", frame)
-
-frame = cv2.resize(frame, None, fx=scaling_factorx, fy=scaling_factory, interpolation=cv2.INTER_AREA)
-
-edged_frame = cv2.Canny(frame, 1, 100)
-cv2.imshow('Edged Frame', edged_frame)
+    frame = cv2.resize(frame, None, fx=scaling_factorx, fy=scaling_factory, interpolation=cv2.INTER_AREA)q
+    edged_frame = cv2.Canny(frame, 1, 100)
+    cv2.imshow('Edged Frame', edged_frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
 x = []
 y = []
